b84e7715db05cdec170c048eec1e5c8c98ebe496
mkorman <mkormanc@gmail.com>
2017-09-28 10:39:22 +0100

    Fixed "new question" dialog
    - Added a dummy post action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'home#about'
   get 'home/index'
+  post 'questions' => 'home#temp'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
